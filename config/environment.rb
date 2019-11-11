77af21a9abc23830a4f02c25b0c95fcbca19baef
=Danial Asghar <=danialasghar5@yahoo.com>
2019-11-11 18:57:48 +0500

    modification in config/environments.rb file

--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,8 @@
 # Load the Rails application.
 require_relative 'application'
 
+#enable active storage as amazon
+ config.active_storage.service = :amazon
+
 # Initialize the Rails application.
 Rails.application.initialize!
